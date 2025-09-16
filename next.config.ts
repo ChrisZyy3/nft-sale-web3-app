b3bc02092f20016875bf1a462934f6ce91fe0713
周雨阳 <zhouyuyang@xyzq.com.cn>
2025-09-16 18:29:30 +0800

    remove extra params

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  swcMinify: true,
-  fastRefresh: true,
-  concurrentFeatures: true,
+  // swcMinify: true,
+  // fastRefresh: true,
+  // concurrentFeatures: true,
 };
 
 export default nextConfig;
